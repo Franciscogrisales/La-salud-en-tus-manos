614ae8586d8391a93cd18150a40bb15ba295d82b
Franciscogrisales <Francisco.grisales@uniminuto.edu.co>
2025-04-23 20:06:29 -0500

    actualizacion

--- public/script.js
+++ public/script.js
@@ -51,7 +51,7 @@ function loadBooks() {
     const bookList = document.getElementById("bookList");
     if (!bookList) return;
 
-    fetch('https://la-salud-en-tus-manos.vercel.app/api/libros')
+    fetch('https://la-salud-en-tus-manos.onrender.com/api/libros')
         .then(response => response.json())
         .then(books => {
             bookList.innerHTML = '';
